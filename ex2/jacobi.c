fdcaacdb19300bf2e98b521c830cd8806a34ba07
Chromeilion <uki@cro.moe>
2025-07-31 23:48:03 +0200

    debugging etc.

--- ex2/jacobi.c
+++ ex2/jacobi.c
@@ -1,6 +1,11 @@
 //
 // Created by chromeilion on 10/5/24.
 //
+#define KERNEL_WIDTH 3
+#define KERNEL_HEIGHT 3
+#define KERNEL_SIZE (KERNEL_WIDTH * KERNEL_HEIGHT)
+#define KERNEL {0.0f, 0.25f, 0.0f, 0.25f, 0.0f, 0.25f, 0.0f, 0.25f, 0.0f}
+
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +14,7 @@
 #include <math.h>
 #include <openacc.h>
 #ifdef USE_GPU
-#include <cublas_v2.h>
+#include <npp.h>
 #endif
 #ifdef USE_BLAS
 #include <cblas.h>
@@ -35,7 +40,7 @@ void print_par(const float * mat, const int n_rows, const int n_cols,
                const int rank, const int npes, FILE *output) {
     int count;
     if(rank)
-        MPI_Send(mat, n_cols*n_rows, MPI_DOUBLE, 0, rank, MPI_COMM_WORLD);
+        MPI_Send(mat, n_cols*n_rows, MPI_FLOAT, 0, rank, MPI_COMM_WORLD);
     else{
         float * buf = (float *) calloc(n_cols*n_rows, sizeof(float));
         if (npes == 1) {
@@ -44,7 +49,7 @@ void print_par(const float * mat, const int n_rows, const int n_cols,
             print_loc(mat, n_rows-1, n_cols, 0, output);
         }
         for( count = 1; count < npes; count ++){
-            MPI_Recv( buf, n_cols*n_rows, MPI_DOUBLE, count, count, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            MPI_Recv( buf, n_cols*n_rows, MPI_FLOAT, count, count, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
             if (count == npes - 1) {
                 print_loc(buf, n_rows, n_cols, 1, output);
                 continue;
@@ -126,37 +131,14 @@ void fill_matr(float *mat, int rows, int cols, float val) {
     }
 }
 
-#ifdef USE_GPU
-void convolve(float *mat, float *res, float *kernel, float *colbuf,
-              int n_rows, int n_cols, int colbuf_rows,
-              int ksize, int stride, int pad, float alpha, float beta,
-              float *topfill, float *botfill, float *leftfill, cublasHandle_t handle) {
-    // Prepare input image for GEMM
-    im2col(mat, n_rows, n_cols, ksize, stride, pad, colbuf,
-           topfill, botfill, leftfill);
-    int m, n, k;
-    m = 1;
-    n = colbuf_rows;
-    k = ksize * ksize;
-    #pragma acc host_data use_device(colbuf, kernel, res)
-    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, &alpha, colbuf, n,
-                kernel, k, &beta, res, n);
+inline void do_j_op(float *mat_new, float *mat, const int i, const int j,
+                    const int n_cols_global) {
+    mat_new[i*n_cols_global + j] =
+            0.25 * (mat[i*n_cols_global + j + 1] +
+                    mat[i*n_cols_global + j-1] +
+                    mat[(i-1)*n_cols_global + j] +
+                    mat[(i+1)*n_cols_global + j]);
 }
-#endif
-#ifdef USE_BLAS
-void convolve(float *mat, float *res, float *kernel, float *colbuf,
-              int n_rows, int n_cols, int colbuf_rows,
-              int ksize, int stride, int pad, float alpha, float beta,
-              float *topfill, float *botfill, float *leftfill) {
-    // Prepare input image for GEMM
-    im2col(mat, n_rows, n_cols, ksize, stride, pad, colbuf,
-           topfill, botfill, leftfill);
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 1, colbuf_rows,
-                ksize*ksize, alpha, kernel, ksize*ksize, colbuf, colbuf_rows, beta, res,
-                colbuf_rows);
-}
-#endif
-
 
 int main( int argc, char * argv[] ) {
     int size;
@@ -164,8 +146,8 @@ int main( int argc, char * argv[] ) {
 
     double start_time;
 
-    float *mat, *mat_new, *colbuf, *botpad, *toppad, *leftpad, *kernel;
-    int i;
+    float *mat, *mat_new;
+    int i, j;
 
     // For communicating with the process above and below me.
     MPI_Comm top_com, bot_com;
@@ -198,9 +180,6 @@ int main( int argc, char * argv[] ) {
         save_to_file = atoi(argv[4]);
     }
 
-#ifdef USE_GPU
-    cublasHandle_t handle;
-    cublasStatus_t status = cublasCreate(&handle);
     int ngpu = acc_get_num_devices(acc_device_nvidia);
     if (ngpu == 0) {
         fprintf(stdout, "No GPU's found!!!");
@@ -208,21 +187,17 @@ int main( int argc, char * argv[] ) {
     }
     acc_set_device_num(rank % ngpu, acc_device_nvidia);
     acc_init(acc_device_nvidia);
-#endif
+    NppStatus npp_status;
 
     // Add 2 because of the static values on the edges
-    const int n_cols_global = size;
-    const int n_rows_global = size;
+    const int n_cols_global = size + 2;
+    const int n_rows_global = size + 2;
     const int top_color = rank + 1;
     const int bot_color = rank;
-    const int n_rows_loc = size / npes;
+    const int n_rows_loc = size / npes + 2;
 
     // Convolution parameters
     const int ksize = 3;
-    const int stride = 1;
-    const int pad = 1;
-    const float alpha = 1.0;
-    const float beta = 0.0;
 
     fprintf(stdout, "%i 0 s | detected %i processes and %i rows per proc. "
                     "n_cols_global is %i\n",
@@ -232,11 +207,6 @@ int main( int argc, char * argv[] ) {
     MPI_Comm_split(MPI_COMM_WORLD, bot_color, rank, &bot_com);
 
     const int mat_size = n_cols_global * n_rows_loc;
-    const int height_col_l = ((n_rows_loc - 2) - ksize) / stride + 1 + 2 * pad;
-    const int width_col = (n_cols_global - ksize) / stride + 1 + 2 * pad;
-    const int buf_rows_l = height_col_l * width_col;
-    const int buf_cols_l = ksize * ksize;
-    int colbuf_size = buf_cols_l * buf_rows_l;
 
     MPI_Barrier(MPI_COMM_WORLD);
 
@@ -245,38 +215,32 @@ int main( int argc, char * argv[] ) {
 
     mat = (float *) calloc(mat_size, sizeof(float));
     mat_new = (float *) calloc(mat_size, sizeof(float));
-    colbuf = (float *) calloc(colbuf_size, sizeof(float));
-    toppad =  (float *) calloc(n_cols_global, sizeof(float));
-    botpad =  (float *) calloc(n_cols_global, sizeof(float));
-    leftpad =  (float *) calloc(n_rows_loc, sizeof(float));
-    kernel = (float *) calloc(9, sizeof(float));
-    kernel[0] = 0.; kernel[1] = 0.25; kernel[2] = 0.; kernel[3] = 0.25;
-    kernel[4] = 0.; kernel[5] = 0.25; kernel[6] = 0.; kernel[7] = 0.25;
-    kernel[8] = 0.;
+    float kernel[KERNEL_SIZE] = KERNEL;
 
-    const float scale_factor = (float) 100 / ((float) (n_rows_global+1));
+    NppiSize oSize = {n_cols_global-2, n_rows_loc-4};
+    NppiSize k_size = {3, 3};
+    NppiPoint anchor = {1, 1};
+    const float scale_factor = (float) 100 / (float)n_rows_global;
     const int global_top_row = n_rows_loc * rank + 1;
 
     // Switch to the accelerator
-    #pragma acc enter data create(mat[0:mat_size], mat_new[0:mat_size], colbuf[0:colbuf_size], toppad[0:n_cols_global], botpad[0:n_cols_global], leftpad[0:n_rows_loc]) copyin(kernel[0:ksize*ksize])
+    #pragma acc enter data create(mat[0:mat_size], mat_new[0:mat_size]), copyin(kernel[0:ksize*ksize])
 
     fprintf(stdout, "%i %f s | initialized local matrix memory\n",
             rank, (double) (clock() - start_time) / CLOCKS_PER_SEC);
 
-    fill_matr(mat, n_rows_loc, n_cols_global, 0.5);
-    fill_matr(toppad, 1, n_cols_global, 0);
     if (rank == npes-1) {
         // If we're the bottom process, we need to fill in the bottom section.
-        #pragma acc parallel loop present(botpad)
+        #pragma acc parallel loop collapse(1) present(mat)
         for (i = 0; i < n_cols_global; i++) {
-            botpad[i] = (float)100 - scale_factor * (float)i;
+            mat[(n_rows_loc-1)*n_cols_global + i] = (float)100 - scale_factor * (float)i;
+            mat_new[(n_rows_loc-1)*n_cols_global + i] = (float)100 - scale_factor * (float)i;
         }
-    } else {
-        fill_matr(botpad, 1, n_cols_global, 0);
     }
-    #pragma acc parallel loop collapse(1) present(leftpad)
-    for (i = 0; i < n_rows_loc; i++) {
-        leftpad[i] = scale_factor * ((float)i+global_top_row);
+    #pragma acc parallel loop collapse(1) present(mat)
+    for (i = 0; i < n_rows_loc - 1; i++) {
+        mat[i*n_cols_global] = scale_factor * ((float)i+global_top_row);
+        mat_new[i*n_cols_global] = scale_factor * ((float)i+global_top_row);
     }
 
     fprintf(stdout, "%i %f s | start idx is %i with %i processes and %i rows "
@@ -288,7 +252,7 @@ int main( int argc, char * argv[] ) {
 #ifndef NDEBUG
     fflush(stdout);
     MPI_Barrier(MPI_COMM_WORLD);
-#pragma acc update self(mat[0:mat_size])
+    #pragma acc update self(mat[0:mat_size])
     print_par(mat, n_rows_loc, n_cols_global, rank, npes, stdout);
     fflush(stdout);
 #endif
@@ -300,57 +264,41 @@ int main( int argc, char * argv[] ) {
     while (iter < max_iter) {
         #pragma acc host_data use_device(mat, mat_new)
         MPI_Iallreduce(
-                mat,
-                mat_new,
+                &mat[n_cols_global],
+                &mat_new[n_cols_global],
                 n_cols_global, MPI_FLOAT,
                 MPI_SUM, top_com, &top_request);
 
         #pragma acc host_data use_device(mat, mat_new)
         MPI_Iallreduce(
-                &mat[(n_rows_loc - 1) * n_cols_global],
-                &mat_new[(n_rows_loc - 1) * n_cols_global],
+                &mat[(n_rows_loc - 2) * n_cols_global],
+                &mat_new[(n_rows_loc - 2) * n_cols_global],
                 n_cols_global,
                 MPI_FLOAT, MPI_SUM, bot_com, &bottom_request);
 
-        // Do our Jacobi iteration step
-#ifdef USE_GPU
-        convolve(&mat[n_cols_global], &mat_new[n_cols_global], kernel,
-                 colbuf, n_rows_loc-2, n_cols_global,
-                 buf_rows_l, ksize, stride, pad, alpha, beta, mat,
-                 &mat[(n_rows_loc-1)*n_cols_global], &leftpad[1], handle);
-#endif
-#ifdef USE_BLAS
-        convolve(&mat[n_cols_global], &mat_new[n_cols_global], kernel,
-                 colbuf, n_rows_loc-2, n_cols_global,
-                 buf_rows_l, ksize, stride, pad, alpha, beta, mat,
-                 &mat[(n_rows_loc-1)*n_cols_global], &leftpad[1]);
-#endif
+        #pragma acc host_data use_device(mat, mat_new, kernel)
+        npp_status = nppiFilter_32f_C1R(&mat[(n_cols_global*1)],
+                           sizeof(float)*n_cols_global,
+                           &mat_new[(n_cols_global*2)+1],
+                           sizeof(float)*n_cols_global, oSize, kernel,
+                           k_size, anchor);
+
+        if (npp_status != NPP_SUCCESS) {
+            fprintf(stderr, "Rank %d encountered NPP Error: %d\n", rank, npp_status);
+        }
+
         // Wait for our Allreduce's to complete
         MPI_Wait(&top_request, MPI_STATUS_IGNORE);
         MPI_Wait(&bottom_request, MPI_STATUS_IGNORE);
-#ifdef USE_GPU
-        convolve(mat, mat_new, kernel,
-                 colbuf, 1, n_cols_global,
-                 n_cols_global, ksize, stride, pad, alpha, beta,
-                 toppad, &mat[n_cols_global], leftpad, handle);
-        convolve(&mat[n_cols_global*(n_rows_loc-1)],
-                 &mat_new[n_cols_global*(n_rows_loc-1)], kernel,
-                 colbuf, 1, n_cols_global, n_cols_global,
-                 ksize, stride, pad, alpha, beta, &mat[n_cols_global*(n_rows_loc-2)],
-                 botpad, &leftpad[n_rows_loc-1], handle);
-#endif
-#ifdef USE_BLAS
-        convolve(mat, mat_new, kernel,
-                 colbuf, 1, n_cols_global,
-                 n_cols_global, ksize, stride, pad, alpha, beta,
-                 toppad, &mat[n_cols_global], leftpad);
 
-        convolve(&mat[n_cols_global*(n_rows_loc-1)],
-                 &mat_new[n_cols_global*(n_rows_loc-1)], kernel,
-                 colbuf, 1, n_cols_global, n_cols_global,
-                 ksize, stride, pad, alpha, beta, &mat[n_cols_global*(n_rows_loc-2)],
-                 botpad, &leftpad[n_rows_loc-1]);
-#endif
+        #pragma acc wait
+
+        // Update the top and bottom rows since we have what we need now
+        #pragma acc parallel loop independent present(mat, mat_new)
+        for (j = 1; j < n_cols_global-1; j++) {
+            do_j_op(mat_new, mat, 1, j, n_cols_global);
+            do_j_op(mat_new, mat, n_rows_loc-2, j, n_cols_global);
+        }
         swap_ptrs(&mat, &mat_new);
         iter += 1;
     }
@@ -370,7 +318,6 @@ int main( int argc, char * argv[] ) {
     print_par(mat, n_rows_loc, n_cols_global, rank, npes, out);
     fclose(out);
     MPI_Finalize();
-    free(mat); free(mat_new); free(colbuf); free(toppad);
-    free(botpad); free(leftpad); free(kernel);
+    free(mat); free(mat_new);
     return 0;
 }
